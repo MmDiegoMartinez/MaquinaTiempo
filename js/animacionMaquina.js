6d8265bec48939d2cf7bbff67b4616dbe456957d
Diego <santiagodiego665@gmail.com>
2024-05-02 14:06:40 -0600

    Se agregó un efecto de rotación  durante el despegue y aterrizaje de la máquina del tiempo.

--- js/animacionMaquina.js
+++ js/animacionMaquina.js
@@ -3,8 +3,8 @@ function animarMovimiento(callback) {
     var viajante = document.getElementById('viajante');
     var maquina = document.querySelector('[gltf-model="#maquina-modelo"]');
     var posicionInicialViajante = { x: 0, y: 0, z: 0 }; // posicion inicial del viajante
-    var posicionIntermediaViajante = { x: 3, y: 0, z: 1.5 }; // Posicion intermedia del viajante
-    var posicionFinalViajante = { x: 3, y: 2, z: 1.5 }; // posicion final del viajante
+    var posicionIntermediaViajante = { x: 3, y: 0.5, z: 1.5 }; // Posicion intermedia del viajante
+    
     var duracionTotalViajante = 3000; // duracion del movimiento del viajante esto en milisegundos
     var pasoViajante = 10; // intervalo de tiempo entre cada paso del viajante en milisegundos
     var tiempoPasadoViajante = 0;
@@ -26,15 +26,17 @@ function animarMovimiento(callback) {
         if (tiempoPasadoViajante < duracionTotalViajante) {
             setTimeout(moverViajante, pasoViajante);
         } else {
+            viajante.setAttribute('visible', 'false');
             // Cuando el viajante alcanza su posicion final, comienza a mover la maquina del tiempo
             moverMaquina(callback);
         }
+
     }
 
     // funcion para mover la maquina del tiempo y al viajante al mismo tiempo
-    function moverMaquina(callback) {
+    function moverMaquina() {
         var posicionInicialMaquina = { x: 0, y: 3, z: -5 }; // posicion inicial de la maquina del tiempo
-        var posicionFinalMaquina = { x: 0, y: 15, z: -5 }; // posicion final de la maquina del tiempo
+        var posicionFinalMaquina = { x: 0, y: 31, z: -5 }; // posicion final de la maquina del tiempo
         var duracionTotalMaquina = 3000; // Duracion total del movimiento de la maquina del tiempo en milisegundos
         var pasoMaquina = 10; // Intervalo de tiempo entre cada paso de la maquina del tiempo en milisegundos
         var tiempoPasadoMaquina = 0;
@@ -45,15 +47,12 @@ function animarMovimiento(callback) {
             var x = posicionInicialMaquina.x + (posicionFinalMaquina.x - posicionInicialMaquina.x) * (tiempoPasadoMaquina / duracionTotalMaquina);
             var y = posicionInicialMaquina.y + (posicionFinalMaquina.y - posicionInicialMaquina.y) * (tiempoPasadoMaquina / duracionTotalMaquina);
             var z = posicionInicialMaquina.z + (posicionFinalMaquina.z - posicionInicialMaquina.z) * (tiempoPasadoMaquina / duracionTotalMaquina);
+            // Aplica la rotación en forma de espiral
+            var rotationY = 360 * (tiempoPasadoMaquina / duracionTotalMaquina);
+            var rotationZ = 360 * (tiempoPasadoMaquina / duracionTotalMaquina);
 
-            // Actualiza la posicion de la maquina del tiempo
             maquina.setAttribute('position', x + ' ' + y + ' ' + z);
-
-            // Calcula la posicion actual del viajante
-            var vy = posicionFinalViajante.y + (15 - posicionFinalViajante.y) * (tiempoPasadoMaquina / duracionTotalMaquina);
-
-            // Actualiza la posicion del viajante
-            viajante.setAttribute('position', posicionFinalViajante.x + ' ' + vy + ' ' + posicionFinalViajante.z);
+            maquina.setAttribute('rotation', '0 ' + rotationY + ' ' + rotationZ);
 
             // Incrementa el tiempo pasado de la maquina del tiempo
             tiempoPasadoMaquina += pasoMaquina;
@@ -65,7 +64,6 @@ function animarMovimiento(callback) {
                 // Cuando la maquina del tiempo llega a su posicion final, desaparece junto con el viajante
                 desvanecerElemento(maquina);
                 desvanecerElemento(viajante);
-               
             }
         }
 
@@ -75,7 +73,6 @@ function animarMovimiento(callback) {
 
     // funcion para desvanecer un elemento gradualmente y mostrar un destello de luz
     function desvanecerElemento(elemento) {
-        
         var opacidad = 1;
         var pasoDesvanecimiento = 0.05;
         var duracionDesvanecimiento = 1000;
@@ -93,16 +90,14 @@ function animarMovimiento(callback) {
 
             if (opacidad > 0) {
                 setTimeout(desvanecer, duracionDesvanecimiento * pasoDesvanecimiento);
-                
             } else {
-                elemento.setAttribute('visible', false); // no sea visible
                 var audio = document.getElementById('audio-maquina');
                 audio.pause();
                 audio.currentTime = 0; // Reinicia el audio al principio para la próxima reproducción
+
+                elemento.setAttribute('visible', false); // no sea visible
                 callback(); // Llama al callback despues de que el destello se desvanezca
-                
             }
-            
         }
         
         // Comienza el desvanecimiento
@@ -115,7 +110,6 @@ function animarMovimiento(callback) {
 
 function reproducirAudio() {
     var audio = document.getElementById('audio-maquina');
-   
     audio.play();
 }
 
--- js/aterrizaje.js
+++ js/aterrizaje.js
@@ -1,49 +1,51 @@
 function aterrizarMaquina() {
     reproducirAudio();
+
     var viajante = document.getElementById('viajante');
+    viajante.setAttribute('visible', false); // Oculta el viajante al inicio
     var maquina = document.querySelector('[gltf-model="#maquina-modelo"]');
-    var posicionInicialMaquina = { x: 0, y: 15, z: -5 }; // posicion inicial de la maquina del tiempo
-    var posicionFinalMaquina = { x: 0, y: 3, z: -5 }; // posicion final de la maquina del tiempo
-    var duracionTotalMaquina = 3000; // duracion total del movimiento de la maquina del tiempo en milisegundos
-    var pasoMaquina = 10; // Intervalo de tiempo entre cada paso de la maquina del tiempo en milisegundos
+    
+    var posicionInicialMaquina = { x: 0, y: 31, z: -5 }; // posición inicial de la máquina del tiempo
+    var posicionFinalMaquina = { x: 0, y: 3, z: -5 }; // posición final de la máquina del tiempo
+    var duracionTotalMaquina = 3000; // Duración total del movimiento de la máquina del tiempo en milisegundos
+    var pasoMaquina = 10; // Intervalo de tiempo entre cada paso de la máquina del tiempo en milisegundos
     var tiempoPasadoMaquina = 0;
 
-    // funcion para mover la maquina del tiempo y el viajante al mismo tiempo
-    function moverMaquina() {
-        // Calcula la posicion actual de la maquina del tiempo
+    // Función para mover la máquina del tiempo
+    function mover() {
+        // Calcula la posición actual de la máquina del tiempo
         var x = posicionInicialMaquina.x + (posicionFinalMaquina.x - posicionInicialMaquina.x) * (tiempoPasadoMaquina / duracionTotalMaquina);
         var y = posicionInicialMaquina.y + (posicionFinalMaquina.y - posicionInicialMaquina.y) * (tiempoPasadoMaquina / duracionTotalMaquina);
         var z = posicionInicialMaquina.z + (posicionFinalMaquina.z - posicionInicialMaquina.z) * (tiempoPasadoMaquina / duracionTotalMaquina);
+        // Aplica la rotación en forma de espiral
+        var rotationY = 360 * (tiempoPasadoMaquina / duracionTotalMaquina);
+        var rotationZ = 360 * (tiempoPasadoMaquina / duracionTotalMaquina);
 
-        // Calcular la posicion actual del viajante
-        var vy = 15 + (3 - 15) * (tiempoPasadoMaquina / duracionTotalMaquina);
-
-        // Actualiza la posicion de la maquina del tiempo
         maquina.setAttribute('position', x + ' ' + y + ' ' + z);
+        maquina.setAttribute('rotation', '0 ' + rotationY + ' ' + rotationZ);
 
-        // Actualiza la posicion del viajante
-        viajante.setAttribute('position', '3 ' + vy + ' 1.5');
-
-        // Incrementa el tiempo pasado de la maquina del tiempo
+        // Incrementa el tiempo pasado de la máquina del tiempo
         tiempoPasadoMaquina += pasoMaquina;
 
-        // Si el tiempo pasado de la maquina del tiempo es menor que la duracion total, sigue moviendo
+        // Si el tiempo pasado de la máquina del tiempo es menor que la duración total, sigue moviendo
         if (tiempoPasadoMaquina < duracionTotalMaquina) {
-            setTimeout(moverMaquina, pasoMaquina);
+            setTimeout(mover, pasoMaquina);
         } else {
-            // Cuando la maquina del tiempo llega a su posicion final, el viajante sale de la nave
-            salirDeNave();
+            viajante.setAttribute('visible', true);
+            salirDeNave(viajante);   
         }
     }
 
-    // funcion para que el viajante salga de la nave
+    // Comienza el movimiento de la máquina del tiempo
+    mover();
+
+    // Función para desvanecer un elemento gradualmente y mostrar un destello de luz
     function salirDeNave() {
-        var duracionSalida = 2000; // duracion de la animacion de salida en milisegundos
-        var pasos = 100; // numero de pasos para la animacion
+        var duracionSalida = 2000; // Duración de la animación de salida en milisegundos
+        var pasos = 100; // Número de pasos para la animación
         var pasoX = (0 - 3) / pasos; // Paso en el eje X
         var pasoY = (0 - 0) / pasos; // Paso en el eje Y
         var pasoZ = (0 - 1.5) / pasos; // Paso en el eje Z
-    
 
         function moverSalida(paso) {
             if (paso <= pasos) {
@@ -55,26 +57,23 @@ function aterrizarMaquina() {
                     moverSalida(paso + 1);
                 }, duracionSalida / pasos);
             } else {
-                
                 var audio = document.getElementById('audio-maquina');
                 audio.pause();
                 audio.currentTime = 0; // Reinicia el audio al principio para la próxima reproducción
             }
         }
     
-        // se Comienza la animacion de salida
+        // Comienza la animación de salida
         moverSalida(1);
     }
 
-    // Comienza el movimiento de la maquina del tiempo
-    moverMaquina();
+    // Comienza el movimiento del viajante
+    moverViajante();
 }
 
 function reproducirAudio() {
     var audio = document.getElementById('audio-maquina');
-   
     audio.play();
 }
 
-// Llama a la funcion para iniciar el aterrizaje de la maquina del tiempo
 aterrizarMaquina();
